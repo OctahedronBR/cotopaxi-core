e44a71dabd0c3a05b645410485f8ee6fcff049d3
Thiago Santos <thiagossantos@gmail.com>
2011-11-12 20:18:32 -0300

    StringConverter: Adds new StringConverter
    Adds a converter that returns the same string passed as the input.
    
    This is merely useful when used on hierarchies/paths that you must have
    a converter for all situations/types.

---
+++ src/main/java/br/octahedron/cotopaxi/controller/converter/StringConverter.java
@@ -0,0 +1,20 @@
+package br.octahedron.cotopaxi.controller.converter;
+
+import br.octahedron.cotopaxi.controller.Converter;
+import br.octahedron.cotopaxi.controller.ConvertionException;
+
+/**
+ * Convenience converter that simply returns the same String, useful for
+ * hierarchies or code paths where you are required to use a converter 
+ * 
+ * @author thiagoss
+ *
+ */
+public class StringConverter implements Converter<String> {
+
+	@Override
+	public String convert(String arg0) throws ConvertionException {
+		return arg0;
+	}
+
+}
